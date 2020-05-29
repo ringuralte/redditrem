2ccfd4ed302a9596bb1641b370e5b28e4404ef3e
ringuralte <ringuralte@gmail.com>
2020-05-29 18:29:37 +0530

    changed authorized js from async/await to normal then catch also using useEffect

--- src/components/Authorize.js
+++ src/components/Authorize.js
@@ -5,9 +5,9 @@ import { useHistory } from "react-router-dom";
 import Loading from "./Loading";
 
 const Authorize = (props) => {
-  console.log('here')
+  console.log("here");
   const history = useHistory();
-  const params = new URLSearchParams(props.location.search).get("state");
+  const state = new URLSearchParams(props.location.search).get("state");
   const error = new URLSearchParams(props.location.search).get("error");
   const code = new URLSearchParams(props.location.search).get("code");
   // const [loading, setLoading] = React.useState(true)
@@ -15,40 +15,40 @@ const Authorize = (props) => {
     `${process.env.REACT_APP_CLIENT_ID}:${process.env.REACT_APP_SECRET}`
   );
 
-  if (error === "access_denied") {
-    history.push("/");
-  }
+  console.log(state)
+  console.log(error)
+  console.log(code)
 
-  if (
-    params === sessionStorage.getItem("uuidState") &&
-    error !== "access_denied"
-  ) {
-    const authorize = async () => {
-      const token = await axios.post(
-        `https://www.reddit.com/api/v1/access_token`,
-        `grant_type=authorization_code&code=${code}&redirect_uri=${
-          process.env.NODE_ENV === "development"
-            ? process.env.REACT_APP_DEV_REDIRECT_URI
-            : process.env.REACT_APP_REDIRECT_URI
-        }`,
-        {
-          headers: {
-            Authorization: `Basic ${encode}`,
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-        }
-      );
-      const data = await token.data;
-      if (data.access_token && !data.error) {
-        sessionStorage.setItem("token", data.access_token);
-        history.push("/home");
-      } else if (data.error === "invalid_grant") {
-        sessionStorage.clear();
-        history.push("/");
-      }
-    };
-    authorize();
-  }
+  React.useEffect(() => {
+    if (state === sessionStorage.getItem("uuidState") && error !== "access_denied") {
+      axios
+        .post(
+          `https://www.reddit.com/api/v1/access_token`,
+          `grant_type=authorization_code&code=${code}&redirect_uri=${
+            process.env.NODE_ENV === "development"
+              ? process.env.REACT_APP_DEV_REDIRECT_URI
+              : process.env.REACT_APP_REDIRECT_URI
+          }`,
+          {
+            headers: {
+              Authorization: `Basic ${encode}`,
+              "Content-Type": "application/x-www-form-urlencoded",
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res)
+          sessionStorage.setItem("token", res.data.access_token);
+          history.push("/home");
+        })
+        .catch((error) => {
+          sessionStorage.clear();
+          history.push("/");
+        });
+    } else if (error === "access_denied") {
+      history.push("/");
+    }
+  }, []);
 
   return <Loading text={"Authorizing your token..."} />;
 };
