2504e09c38dd9093df955ef2b654db79b03fdab1
ringuralte <ringuralte@gmail.com>
2020-06-01 10:57:59 +0530

    changed button to anchor link

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -2,19 +2,22 @@ import React from "react";
 import { uuid } from "uuidv4";
 
 const SignIn = () => {
-  const setUuid = () => {
-    // const state = "7960142923";
-    localStorage.setItem("state", uuid())
+  React.useState(() => {
+    localStorage.setItem("state", uuid());
+  }, []);
+  // const setUuid = () => {
+  //   const state = "7960142923";
+  //   localStorage.setItem("state", uuid());
 
-    window.location.href = `https://www.reddit.com/api/v1/authorize?client_id=${
-      process.env.REACT_APP_CLIENT_ID
-    }&response_type=code&state=${localStorage.getItem("state")}&redirect_uri=${
-      process.env.NODE_ENV === "development"
-        ? process.env.REACT_APP_DEV_REDIRECT_URI
-        : process.env.REACT_APP_REDIRECT_URI
-    }&duration=temporary&scope=history identity save
-    `;
-  };
+  //   window.location.href = `https://www.reddit.com/api/v1/authorize?client_id=${
+  //     process.env.REACT_APP_CLIENT_ID
+  //   }&response_type=code&state=${localStorage.getItem("state")}&redirect_uri=${
+  //     process.env.NODE_ENV === "development"
+  //       ? process.env.REACT_APP_DEV_REDIRECT_URI
+  //       : process.env.REACT_APP_REDIRECT_URI
+  //   }&duration=temporary&scope=history identity save
+  //   `;
+  // };
   return (
     <div className="grid md:grid-cols-2 bg-gray-100 min-h-screen">
       <div className="flex items-center justify-center md:mb-32">
@@ -27,12 +30,25 @@ const SignIn = () => {
       </div>
       <div className="flex items-center flex-col justify-center md:mb-32">
         <div className="pb-32 md:pb-0 flex flex-col items-left">
-          <button
+          {/* <button
             className="text-xl md:text-2xl tracking-wider border-2 rounded-md border-orange-600 hover:bg-orange-600 hover:text-white font-bold text-orange-600 py-4 px-6 md:py-6 md:px-8"
             onClick={setUuid}
+          > */}
+          <a
+            className="text-xl md:text-2xl tracking-wider border-2 rounded-md border-orange-600 hover:bg-orange-600 hover:text-white font-bold text-orange-600 py-4 px-6 md:py-6 md:px-8"
+            href={`https://www.reddit.com/api/v1/authorize?client_id=${
+              process.env.REACT_APP_CLIENT_ID
+            }&response_type=code&state=${localStorage.getItem(
+              "state"
+            )}&redirect_uri=${
+              process.env.NODE_ENV === "development"
+                ? process.env.REACT_APP_DEV_REDIRECT_URI
+                : process.env.REACT_APP_REDIRECT_URI
+            }&duration=temporary&scope=history identity save`}
           >
             LOG IN
-          </button>
+          </a>
+          {/* </button> */}
           <span className="block pt-2 text-gray-800">
             Using your reddit account.
           </span>
