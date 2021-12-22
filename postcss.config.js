894237b0dbe1645f6bc7318e9708f9e8973ba52c
ringuralte <ringuralte@protonmail.com>
2021-12-22 15:41:08 +0530

    Updating dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,6 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: [
-    './public/**/*.html',
-    './src/**/*.js',
-  ],
-
-  defaultExtractor: content => content.match(/[\w0/:]+(?<!:)/g) || []
-})
-
 module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-    ...process.env.NODE_ENV === 'production' ? [purgecss] : []
-  ]
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
